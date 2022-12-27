80c9ceaf427ec3959688c405d7d3d20723787b90
bismo nugroho <bismo.nugroho@cpp.co.id>
2022-12-27 11:23:41 +0700

    init

---
+++ esp32_espnow_sender/esp32_espnow_sender.ino
@@ -0,0 +1,305 @@
+#include <Arduino.h>
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp-now-wi-fi-web-server/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+/*
+JC Servaye
+Complete project details at https://github.com/Servayejc/esp_now_sender/
+
+Automatic pairing for ESP NOW
+
+In the server: 
+- The WiFi channel is determined by your router
+- Don't know the sender MAC
+
+In the senders: 
+- No access to the router
+- Don't know the routeur MAC
+
+The server is supposed running.
+
+The sender set esp now on channel 1
+The server add en entry with the broadcast address to his peer list
+The sender send a pairing request in broadcast
+
+If the server received the message we are on the good channel
+    The server add the received MAC to his peer list
+    The server reply to the MAC address a message containing his channel number
+
+    The sender replace the broadcast address by the server address in his peer list
+
+else 
+    The sender repeat the process on the next channel
+
+Notes: 
+  -13 channels exists in Europe, you can change MAX_CHANNEL value to accomodate your location  
+  - Added boot reason
+  - Added sleep mode 
+
+TODO:
+  In case of server restart, it may use another WiFi channel, we need to add code for this situation. 
+
+*/
+
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include <WiFi.h>
+#include <EEPROM.h>
+
+// Set your Board and Server ID 
+#define BOARD_ID 2
+#define SERVER_ID 0
+#define MAX_CHANNEL 11  // for North America // 13 in Europe
+#define SLEEP_MODE
+
+
+uint8_t serverAddress[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
+//Structure to send data
+//Must match the receiver structure
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+  uint8_t msgType;
+  uint8_t id;
+  float temp;
+  float hum;
+  unsigned int readingId;
+} struct_message;
+
+typedef struct struct_pairing {       // new structure for pairing
+    uint8_t msgType;
+    uint8_t id;
+    uint8_t macAddr[6];
+    uint8_t channel;
+} struct_pairing;
+
+//Create 2 struct_message 
+struct_message myData;  // data to send
+struct_message inData;  // data received
+struct_pairing pairingData;
+
+enum PairingStatus {NOT_PAIRED, PAIR_REQUEST, PAIR_REQUESTED, PAIR_PAIRED,};
+PairingStatus pairingStatus = NOT_PAIRED;
+
+enum MessageType {PAIRING, DATA,};
+MessageType messageType;
+
+RTC_DATA_ATTR int channel = 9;    // save in RTC so in sleep mode we can use directly the good channel  
+RTC_DATA_ATTR unsigned int readingId = 0; 
+RTC_DATA_ATTR int bootCount = 0;
+
+// simulate temperature and humidity data
+float t = 0;
+float h = 0;
+
+unsigned long currentMillis = millis();
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 5000;         // Interval at which to publish sensor readings
+unsigned long start;                // used to measure Pairing time
+   
+
+#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  5           /* Time ESP32 will go to sleep (in seconds) */
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep\n"); break;
+  }
+}
+
+// simulate temperature reading
+float readDHTTemperature() {
+  t += 1;
+  return t;
+}
+
+// simulate humidity reading
+float readDHTHumidity() {
+  h += 10;
+  return h;
+}
+
+void addPeer(const uint8_t * mac_addr, uint8_t chan){
+  esp_now_peer_info_t peer;
+  ESP_ERROR_CHECK(esp_wifi_set_channel(chan ,WIFI_SECOND_CHAN_NONE));
+  esp_now_del_peer(mac_addr);
+  memset(&peer, 0, sizeof(esp_now_peer_info_t));
+  peer.channel = chan;
+  peer.encrypt = false;
+  memcpy(peer.peer_addr, mac_addr, sizeof(uint8_t[6]));
+  if (esp_now_add_peer(&peer) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  memcpy(serverAddress, mac_addr, sizeof(uint8_t[6]));
+}
+
+void printMAC(const uint8_t * mac_addr){
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+}
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) { 
+  Serial.print("Packet received from: ");
+  printMAC(mac_addr);
+  Serial.println();
+  Serial.print("data size = ");
+  Serial.println(sizeof(incomingData));
+  uint8_t type = incomingData[0];
+  switch (type) {
+  case DATA :      // we received data from server
+    memcpy(&inData, incomingData, sizeof(inData));
+    t = inData.temp;
+    h = inData.hum;
+    Serial.print("ID  = ");
+    Serial.println(inData.id);
+    Serial.print("Setpoint temp = ");
+    Serial.println(inData.temp);
+    Serial.print("SetPoint humidity = ");
+    Serial.println(inData.hum);
+    Serial.print("reading Id  = ");
+    Serial.println(inData.readingId);
+    break;
+
+  case PAIRING:    // we received pairing data from server
+    memcpy(&pairingData, incomingData, sizeof(pairingData));
+    if (pairingData.id == 0) {              // the message comes from server
+      printMAC(mac_addr);
+      Serial.print("Pairing done for ");
+      printMAC(pairingData.macAddr);
+      Serial.print(" on channel " );
+      Serial.print(pairingData.channel);    // channel used by the server
+      Serial.print(" in ");
+      Serial.print(millis()-start);
+      Serial.println("ms");
+      addPeer(pairingData.macAddr, pairingData.channel); // add the server  to the peer list 
+      #ifdef SAVE_CHANNEL
+        lastChannel = pairingData.channel;
+        EEPROM.write(0, pairingData.channel);
+        EEPROM.commit();
+      #endif  
+      pairingStatus = PAIR_PAIRED;             // set the pairing status
+    }
+    break;
+  }  
+}
+
+PairingStatus autoPairing(){
+  switch(pairingStatus) {
+    case PAIR_REQUEST:
+    Serial.print("Pairing request on channel "  );
+    Serial.println(channel);
+
+    // set WiFi channel   
+    ESP_ERROR_CHECK(esp_wifi_set_channel(channel,  WIFI_SECOND_CHAN_NONE));
+    if (esp_now_init() != ESP_OK) {
+      Serial.println("Error initializing ESP-NOW");
+    }
+
+    // set callback routines
+    esp_now_register_send_cb(OnDataSent);
+    esp_now_register_recv_cb(OnDataRecv);
+  
+    // set pairing data to send to the server
+    pairingData.msgType = PAIRING;
+    pairingData.id = BOARD_ID;     
+    pairingData.channel = channel;
+
+    // add peer and send request
+    addPeer(serverAddress, channel);
+    esp_now_send(serverAddress, (uint8_t *) &pairingData, sizeof(pairingData));
+    previousMillis = millis();
+    pairingStatus = PAIR_REQUESTED;
+    break;
+
+    case PAIR_REQUESTED:
+    // time out to allow receiving response from server
+    currentMillis = millis();
+    if(currentMillis - previousMillis > 250) {
+      previousMillis = currentMillis;
+      // time out expired,  try next channel
+      //channel ++;
+      if (channel > MAX_CHANNEL){
+         channel = 1;
+      }   
+      pairingStatus = PAIR_REQUEST;
+    }
+    break;
+
+    case PAIR_PAIRED:
+      // nothing to do here 
+    break;
+  }
+  return pairingStatus;
+}  
+
+void setup() {
+  Serial.begin(74880);
+  Serial.println();
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+  print_wakeup_reason();
+  Serial.print("Client Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  start = millis();
+
+  #ifdef SLEEP_MODE 
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
+  #endif
+  
+  pairingStatus = PAIR_REQUEST;
+}  
+
+void sendData(){
+  myData.msgType = DATA;
+  myData.id = BOARD_ID;
+  myData.temp = readDHTTemperature();
+  myData.hum = readDHTHumidity();
+  myData.readingId = readingId++;
+  esp_err_t result = esp_now_send(serverAddress, (uint8_t *) &myData, sizeof(myData));
+}  
+
+void loop() {
+  if (autoPairing() == PAIR_PAIRED) {
+    #ifdef SLEEP_MODE 
+      sendData();
+      Serial.println("Going to sleep now");
+      Serial.flush(); 
+      esp_deep_sleep_start();
+    #else
+      unsigned long currentMillis = millis();
+      if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        sendData();
+      }  
+    #endif
+  }
+}
